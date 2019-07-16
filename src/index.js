a96501cec575da181d3eaba7a056195ea7106fe7
Simen Bekkhus <sbekkhus91@gmail.com>
2019-07-16 15:37:13 +0200

    chore: ignore coverage of copied function

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import { basename, join } from 'path';
 import * as snapshotProcessor from './processors/snapshot-processor';
 
 // copied from https://github.com/babel/babel/blob/56044c7851d583d498f919e9546caddf8f80a72f/packages/babel-helpers/src/helpers.js#L558-L562
+/* istanbul ignore next */
 function interopRequireDefault(obj) {
   return obj && obj.__esModule ? obj : { default: obj };
 }
