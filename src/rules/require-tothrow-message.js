694a8cfc6980f42c8cf0557c202f726c843ebdcc
Chris Blossom <cbloss@gmail.com>
2019-07-18 16:04:06 -0700

    chore(require-tothrow-message): remove meta.docs.replacedBy

--- src/rules/require-tothrow-message.js
+++ src/rules/require-tothrow-message.js
@@ -1,17 +1,11 @@
 import requireToThrowMessage from './require-to-throw-message';
 
-const replacedBy = ['require-to-throw-message'];
-
 const requireTothrowMessage = {
   ...requireToThrowMessage,
   meta: {
     ...requireToThrowMessage.meta,
-    docs: {
-      ...requireToThrowMessage.meta.docs,
-      replacedBy,
-    },
     deprecated: true,
-    replacedBy,
+    replacedBy: ['require-to-throw-message'],
   },
 };
 
